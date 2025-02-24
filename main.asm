74906bf0eabcf509e380a39d1e1bb74237afaa4b
SebastianDC23615 <dua23615@uvg.edu.gt>
2025-02-24 16:57:37 -0600

    Initial commit

---
+++ main.asm
@@ -0,0 +1,14 @@
+; ============================================
+; Universidad del Valle de Guatemala
+; IE2024: Programación de microcontroladores
+; Proyecto 1
+;
+; Created: 24/02/2025
+; Author : Sebastián Duarte Calderón
+; Hardware: ATmega328P
+; Descripción: Proyecto 1, Reloj alarma con multiples modos
+; ============================================
+
+start:
+    inc r16
+    rjmp start
